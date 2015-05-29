9f054045c6506ee9849d8e8314d7ea0ad0880180
Olivier Balais <obalais@gmail.com>
2015-05-29 13:19:01 +0200

    Fuck**g tabs

--- src/Bobey/GitlabIssueByMail/Command/FetchMailCommand.php
+++ src/Bobey/GitlabIssueByMail/Command/FetchMailCommand.php
@@ -11,39 +11,38 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class FetchMailCommand extends Command
 {
-	protected function configure()
-	{
-		$this
-			->setName('gitlab:fetch-mail')
-			->setDescription('Fetch emails from given mail address and create Gitlab Issues from it')
-			->addOption(
-				'token',
-				null,
-				InputOption::VALUE_REQUIRED,
-				'Your gitlab private token'
-			)
-			->addOption(
-				'projectId',
-				null,
-				InputOption::VALUE_REQUIRED,
-				'Your gitlab project ID'
-			)
-		;
-	}
+    protected function configure()
+    {
+        $this
+            ->setName('gitlab:fetch-mail')
+            ->setDescription('Fetch emails from given mail address and create Gitlab Issues from it')
+            ->addOption(
+                'token',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Your gitlab private token'
+            )
+            ->addOption(
+                'projectId',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Your gitlab project ID'
+            );
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$token = $input->getOption('token');
-		$projectId = $input->getOption('projectId');
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $token = $input->getOption('token');
+        $projectId = $input->getOption('projectId');
 
-		$client = new GitlabClient('https://gitlab.com/api/v3/');
-		$client->authenticate($token, GitlabClient::AUTH_URL_TOKEN);
+        $client = new GitlabClient('https://gitlab.com/api/v3/');
+        $client->authenticate($token, GitlabClient::AUTH_URL_TOKEN);
 
-		$project = new GitlabProject($projectId, $client);
+        $project = new GitlabProject($projectId, $client);
 
-		$project->createIssue('This does not work..', array(
-			'description' => 'This doesnt work properly. Please fix!',
-			'assignee_id' => null,
-		));
-	}
+        $project->createIssue('This does not work..', array(
+            'description' => 'This doesnt work properly. Please fix!',
+            'assignee_id' => null,
+        ));
+    }
 }
