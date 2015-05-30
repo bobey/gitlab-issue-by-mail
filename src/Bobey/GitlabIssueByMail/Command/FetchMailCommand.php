059ff6d81d7d35fd22417d1ec3a5cf475fae0d95
Olivier Balais <obalais@gmail.com>
2015-05-30 13:14:03 +0200

    Extracted configuration to yaml file

--- src/Bobey/GitlabIssueByMail/Command/FetchMailCommand.php
+++ src/Bobey/GitlabIssueByMail/Command/FetchMailCommand.php
@@ -2,12 +2,14 @@
 
 namespace Bobey\GitlabIssueByMail\Command;
 
+use Bobey\GitlabIssueByMail\Configuration\ParametersConfiguration;
 use Gitlab\Client as GitlabClient;
 use Gitlab\Model\Project as GitlabProject;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
 
 class FetchMailCommand extends Command
 {
@@ -15,27 +17,24 @@ class FetchMailCommand extends Command
     {
         $this
             ->setName('gitlab:fetch-mail')
-            ->setDescription('Fetch emails from given mail address and create Gitlab Issues from it')
-            ->addOption(
-                'token',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Your gitlab private token'
-            )
-            ->addOption(
-                'projectId',
-                null,
-                InputOption::VALUE_REQUIRED,
-                'Your gitlab project ID'
-            );
+            ->setDescription('Fetch emails from given mail address and create Gitlab Issues from it');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $token = $input->getOption('token');
-        $projectId = $input->getOption('projectId');
+        $yaml = new Parser();
 
-        $client = new GitlabClient('https://gitlab.com/api/v3/');
+        $config = $yaml->parse(file_get_contents( __DIR__ . '/../../../../config/parameters.yml'));
+
+        $processor = new Processor();
+        $configuration = new ParametersConfiguration();
+        $processedConfiguration = $processor->processConfiguration($configuration, [$config]);
+
+        $token = $processedConfiguration['gitlab']['token'];
+        $projectId = $processedConfiguration['gitlab']['projectId'];
+        $gitlabUrl = $processedConfiguration['gitlab']['host'];
+
+        $client = new GitlabClient(sprintf('%s/api/v3/', $gitlabUrl));
         $client->authenticate($token, GitlabClient::AUTH_URL_TOKEN);
 
         $project = new GitlabProject($projectId, $client);
---
+++ src/Bobey/GitlabIssueByMail/Configuration/ParametersConfiguration.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bobey\GitlabIssueByMail\Configuration;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class ParametersConfiguration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('parameters');
+
+        $rootNode
+            ->children()
+
+                ->arrayNode('mail')
+                    ->children()
+                        ->scalarNode('from')->end()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('password')->end()
+                    ->end()
+                ->end()
+
+                ->arrayNode('gitlab')
+                    ->children()
+                        ->scalarNode('host')->end()
+                        ->scalarNode('projectId')->end()
+                        ->scalarNode('token')->end()
+                    ->end()
+                ->end()
+
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
