3d34dd2cb2beb0c33c426d69df90bdcbe64d8a7c
Steven Cook <visage@deadhexagon.com>
2018-10-23 23:32:00 -0400

    Issues are now created in gitlab with output to console

--- src/Velocityorg/GitlabIssueByMail/Command/FetchMailCommand.php
+++ src/Velocityorg/GitlabIssueByMail/Command/FetchMailCommand.php
@@ -6,6 +6,8 @@ use Fetch\Message;
 use Fetch\Server;
 use Gitlab\Client as GitlabClient;
 use Gitlab\Model\Project as GitlabProject;
+//use Gitlab\Model\User as GitlabUser;
+use Gitlab\Api\Users;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,13 +15,17 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Parser;
 
 class FetchMailCommand extends Command {
-    protected function configure() {
+    protected $logger;
+
+    protected function configure()
+    {
         $this
             ->setName('gitlab:fetch-mail')
             ->setDescription('Fetch e-mails from specified address and create Gitlab issue(s) from the result');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $yaml = new Parser();
 
         $config = $yaml->parse(file_get_contents('config.yaml'));
@@ -52,29 +58,109 @@ class FetchMailCommand extends Command {
 
         $project = new GitlabProject($projectId, $client);
 
+        if (count($messages) > 0) {
+            $this->writeOutput($output, sprintf("Found <info>%d</info> new message(s)", count($messages)));
+        } else {
+            $this->writeOutput($output, "No messages found, goodbye");
+        }
+
         foreach ($messages as $message) {
+            //print_r($message->msgno);
+            $headers = $message->getHeaders();
+
             $issueTitle = $message->getSubject();
             $issueContent = $message->getMessageBody();
 
-            $project->createIssue(
-                $issueTitle,
-                [
-                    'description' => $issueContent
-                ]
-            );
+            $email = $this->processAddressObject($message->getHeaders()->from);
+            $userRecord = $this->getUserByEmail($client, $email['address']);
 
-            if ($output->getVerbosity() <= OutputInterface::VERBOSITY_VERBOSE) {
-                $output->writeln(sprintf('<info>Created a new issue: <comment>%s</comment></info>', $issueTitle));
+            // Only continue if we have a matching user record
+            if ($userRecord) {
+                $this->writeOutput($output, sprintf("E-mail from <comment>%s</comment> : <info>Matched user </info><comment>%s</comment>", $email['address'], $userRecord['username'] . ' (' . $userRecord['name']. ')'));
+
+                $result = $project->createIssue(
+                    $issueTitle,
+                    [
+                        'description' => $issueContent
+                    ]
+                );
+
+                /*
+                 * Update issue in the database
+                 *
+                 * UPDATE ISSUES SET '' = '' WHERE '' = '';
+                 */
+                //print_r($result->id);
+                //print_r($result->project_id);
+                //print_r($result->author->show());
+
+                $this->writeOutput($output, sprintf('Created new issue %d : <info>%s</info>', $result->iid, $issueTitle));
+            } else {
+                $this->writeOutput($output, sprintf("E-mail from <comment>%s</comment> : <error>User not found, skipping</error></info>", $email['address']));
             }
 
-            $message->delete();
+            $this->writeOutput($output, sprintf('Deleting e-mail message <info>%d</info>', $message->getOverview()->msgno));
+
+            /*if ($output->getVerbosity() <= OutputInterface::VERBOSITY_VERBOSE) {
+            //    $output->writeln(sprintf('<info>Created a new issue: <comment>%s</comment></info>', $issueTitle));
+            }*/
+
+            //$message->delete();
         }
 
-        $output->writeln(count($messages) ?
-            sprintf('<info>Created %d new issue%s</info>', count($messages), count($messages) > 1 ? 's' : '') :
-            '<info>No new issue created</info>'
+        //$output->writeln(count($messages) ?
+        //    sprintf('<info>Created %d new issue%s</info>', count($messages), count($messages) > 1 ? 's' : '') :
+        //    '<info>No new issue created</info>'
+        //);
+
+        // Expunge deleted e-mail messages
+        $server->expunge();
+    }
+
+    protected function processAddressObject($addresses) {
+        $outputAddresses = [];
+
+        if (is_array($addresses))
+        {
+            foreach ($addresses as $address) {
+                if (property_exists($address, 'mailbox') && $address->mailbox != 'undisclosed-recipients')
+                {
+                    $currentAddress = [];
+                    $currentAddress['address'] = $address->mailbox . '@' . $address->host;
+
+                    if (isset($address->personal))
+                    {
+                        $currentAddress['name'] = $address->personal;
+                    }
+
+                    $outputAddresses[] = $currentAddress;
+                }
+            }
+        }
+
+        return $outputAddresses[0];
+    }
+
+    protected function getUserByEmail($client, $email)
+    {
+        $users = new Users($client);
+
+        // Find the user with the given e-mail address
+        $user = $users->all(
+            [
+                'search' => $email
+            ]
         );
 
-        $server->expunge();
+        return (is_array($user) && count($user) > 0) ? $user[0] : false;
+    }
+
+    protected function writeOutput(OutputInterface $output, $message)
+    {
+        $timestamp = date('Y-m-d h:i:s');
+
+        if ($output->getVerbosity() <= OutputInterface::VERBOSITY_VERBOSE) {
+            $output->writeln("${timestamp} ${message}");
+        }
     }
 }
